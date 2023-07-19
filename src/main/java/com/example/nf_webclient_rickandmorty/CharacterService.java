56cce8ad50fd62174f7af91ac2e9860de4d48d46
mrg <rgmengedoth@gmail.com>
2023-07-19 14:46:22 +0200

    changes

--- src/main/java/com/example/nf_webclient_rickandmorty/CharacterService.java
+++ src/main/java/com/example/nf_webclient_rickandmorty/CharacterService.java
@@ -1,19 +1,15 @@
 package com.example.nf_webclient_rickandmorty;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
-import java.util.Objects;
+
 
 @Service
 @RequiredArgsConstructor
 public class CharacterService {
-
-    private final WebClient webClient = WebClient.create("https://rickandmortyapi.com/api");
-    private ResponseEntity<CharacterResponse> responseEntity;
 
     public List<Character> getAllChar(){
         WebClient webClient1 = WebClient.create("https://rickandmortyapi.com/api");
@@ -24,9 +20,9 @@ public class CharacterService {
                 .toEntity(CharacterResponse.class)
                 .block()
                 .getBody();
+        assert response != null;
         return response.getResults();
     }
-
 
     public List<Character> getAllCharAlive(){
         WebClient webClient1 = WebClient.create("https://rickandmortyapi.com/api");
@@ -37,6 +33,7 @@ public class CharacterService {
                 .toEntity(CharacterResponse.class)
                 .block()
                 .getBody();
+        assert response != null;
         return response.getResults();
     }
 
